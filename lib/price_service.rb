4243e31bbc53bd6b8967757b9366858ea9d9527f
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2018-01-03 12:45:29 -0500

    get price from API

--- lib/price_service.rb
+++ lib/price_service.rb
@@ -1,10 +1,15 @@
+require 'httparty'
+
 class PriceService
   KEY = '6GCV80279I2N8Y5A'
-  FUNCTION = 'TIME_SERIES_INTRADAY'
+  FUNCTION = 'TIME_SERIES_DAILY'
   INTERVAL = '1min'
-  URL = "https://www.alphavantage.co/query?function=#{FUNCTION}&interval=#{INTERVAL}&apikey=#{KEY}&symbol="
+  URL = "https://www.alphavantage.co/query?function=#{FUNCTION}&apikey=#{KEY}&symbol="
+  PRICES = "Time Series (Daily)"
+  PRICE = "4. close"
 
   def self.read(symbol)
-    HTTParty.get(URL + symbol).body
+    data = JSON.parse(HTTParty.get(URL + symbol).body)
+    data[PRICES][Date.today.to_s][PRICE].to_f
   end
 end
