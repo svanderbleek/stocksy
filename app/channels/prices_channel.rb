87d1f5c7ab26281f041d1c3c33a9821a0fc129cd
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2018-01-03 18:39:56 -0500

    Update prices_channel.rb

--- app/channels/prices_channel.rb
+++ app/channels/prices_channel.rb
@@ -2,8 +2,4 @@ class PricesChannel < ApplicationCable::Channel
   def subscribed
     stream_from Portfolio.default_channel
   end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
